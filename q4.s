8da006c2967585983b6711fcefe48520851c4cde
cs17b008 <46598027+cs17b008@users.noreply.github.com>
2019-03-15 15:12:06 +0530

    Update q4.s

--- q4.s
+++ q4.s
@@ -10,7 +10,7 @@ main:
 	lw $t1,q
 	lw $s7,a
 	lw $s8,r
-	addi $s6,$s6,29
+	addi $s6,$s6,32
 	addi $s0,$s0,0
 	addi $s1,$s1,1
 	addi $t3,$t3,0
@@ -25,20 +25,20 @@ loop:
 
 
 loop1:
-	add $s7,$t0,$s7
+	addu $s7,$t0,$s7
 	and $s4,$s7,$s1
 	sub $s6,$s6,$s1
-	sll $s4,$s4,30
-	add $s8,$s8,$s4
+	sll $s4,$s4,$s6
+	addu $s8,$s8,$s4
 	srl $s7,$s7,1
-	add $t3,$t3,$s1
+	addu $t3,$t3,$s1
 	j loop
 
 loop2:
 	and $s5,$s7,$s1
 	sllv $s5,$s5,$s6
 	sub $s6,$s6,$s1
-	add $s8,$s8,$s5
+	addu $s8,$s8,$s5
 	srl $s7,$s7,1
 	add $t3,$t3,$s1
 	j loop
